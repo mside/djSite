3a79a65d8cfb454bbe32bd74d382f53ee9c6fc65
mside <michaelside0@gmail.com>
2018-04-19 18:13:43 -0700

    finished gallery

--- js/vendor/masonry.js
+++ js/vendor/masonry.js
@@ -1,5 +1,7 @@
-$('.grid').masonry({
-  itemSelector: '.grid-item', // use a separate class for itemSelector, other than .col-
-  columnWidth: '.grid-sizer',
-  percentPosition: true
-});
+
+    $('.grid').masonry({
+        itemSelector: '.grid-item', // use a separate class for itemSelector, other than .col-
+        columnWidth: '.grid-sizer',
+        percentPosition: true
+    });
+
